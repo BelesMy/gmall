a711405b5a152cb6450cb99c5e907efbaebc80c7
smallAaa <2191759543@qq.com>
2021-10-22 14:35:41 +0800

    创建user整体

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserManageController.java
@@ -0,0 +1,29 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.service.UserManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserManageController {
+    @Autowired
+    UserManageService userService;
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String getIndex() {
+        return "hello word";
+    }
+
+    @RequestMapping("/getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers = userService.getAllUser();
+        return umsMembers;
+    }
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserManageServiceImpl.java
@@ -0,0 +1,20 @@
+package com.atguigu.gmall.user.service.impl;
+
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import com.atguigu.gmall.user.service.UserManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserManageServiceImpl implements UserManageService {
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public List<UmsMember> getAllUser() {
+        return userMapper.selectAllUser();
+    }
+}
