52abc7635bdb3e97b347c56a5a49b913eb237795
Administrator <821512040@qq.com>
2021-10-28 01:26:12 +0800

    20211028  sku

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -4,9 +4,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsProductInfo;
 import com.atguigu.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,10 +14,22 @@ import java.util.List;
 public class SpuController {
     @Reference
     SpuService spuService;
-    @RequestMapping("spuList")
+    @RequestMapping("/spuList")
     @ResponseBody
     public List<PmsProductInfo> spuList (String catalog3Id){
         List<PmsProductInfo> spuList = spuService.spuList(catalog3Id);
         return spuList;
+    }
+    @RequestMapping("/saveSpuInfo")
+    @ResponseBody
+    public List<PmsProductInfo> saveSpuInfo (@RequestBody PmsProductInfo pmsProductInfo ){
+
+        return null;
+    }
+
+    @RequestMapping("fileUpload")
+    @ResponseBody
+    public String fileUpload(@RequestParam("file") MultipartFile multipartFile){
+        return "https://img14.360buyimg.com/n0/jfs/t1/199270/22/8292/88751/61418b0cE7a752547/8584becc705d8e69.jpg";
     }
 }
