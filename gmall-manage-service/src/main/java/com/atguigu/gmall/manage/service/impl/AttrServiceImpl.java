55536e93ef8b46da30c5b1fe5bbbdffcfb14f2ef
Administrator <821512040@qq.com>
2021-10-27 15:00:30 +0800

    20211027  sku

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/AttrServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/AttrServiceImpl.java
@@ -6,7 +6,9 @@ import com.atguigu.gmall.bean.PmsBaseAttrValue;
 import com.atguigu.gmall.manage.mapper.PmsBaseAttrInfoMapper;
 import com.atguigu.gmall.manage.mapper.PmsBaseAttrValueMapper;
 import com.atguigu.gmall.service.AttrService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 @Service
@@ -24,12 +26,35 @@ public class AttrServiceImpl implements AttrService {
 
     @Override
     public String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo) {
-        pmsBaseAttrInfoMapper.insertSelective(pmsBaseAttrInfo);
-        List<PmsBaseAttrValue> list = pmsBaseAttrInfo.getAttrValueList();
-        for(PmsBaseAttrValue pmsBaseAttrValue : list){
+        if(StringUtils.isBlank(pmsBaseAttrInfo.getId())){
+            pmsBaseAttrInfoMapper.insertSelective(pmsBaseAttrInfo);
+            List<PmsBaseAttrValue> list = pmsBaseAttrInfo.getAttrValueList();
+            for(PmsBaseAttrValue pmsBaseAttrValue : list){
+                pmsBaseAttrValue.setAttrId(pmsBaseAttrInfo.getId());
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            }
+        }else{
+            //修改PmsBaseAttrInfo  属性
+            Example ex =  new Example(PmsBaseAttrInfo.class);
+            ex.createCriteria().andEqualTo("id",pmsBaseAttrInfo.getId());
+            pmsBaseAttrInfoMapper.updateByExampleSelective(pmsBaseAttrInfo,ex);
+            //删除属性值
+            PmsBaseAttrValue pmsBaseAttrValue = new PmsBaseAttrValue();
             pmsBaseAttrValue.setAttrId(pmsBaseAttrInfo.getId());
-            pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            pmsBaseAttrValueMapper.delete(pmsBaseAttrValue);
+            //新增属性值
+            for (PmsBaseAttrValue pmsBaseAttrValuenew :pmsBaseAttrInfo.getAttrValueList()){
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValuenew);
+            }
         }
+
         return  "success";
+    }
+
+    @Override
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId) {
+        PmsBaseAttrValue pmsBaseAttrValue = new PmsBaseAttrValue();
+        pmsBaseAttrValue.setAttrId(attrId);
+        return pmsBaseAttrValueMapper.select(pmsBaseAttrValue);
     }
 }
--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
@@ -2,6 +2,7 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
 import com.atguigu.gmall.service.AttrService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,7 +16,7 @@ import java.util.List;
 public class AttrController {
     @Reference
     AttrService attrService;
-    @RequestMapping()
+    @RequestMapping("/attrInfoList")
     @ResponseBody
     public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id){
         return attrService.attrInfoList(catalog3Id);
@@ -27,6 +28,12 @@ public class AttrController {
     public String saveAttrInfo(@RequestBody PmsBaseAttrInfo pmsBaseAttrInfo ){
         String result = attrService.saveAttrInfo(pmsBaseAttrInfo);
         return  "success";
+    }
+
+    @RequestMapping("/getAttrValueList")
+    @ResponseBody
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId){
+        return  attrService.getAttrValueList(attrId);
     }
 
 
